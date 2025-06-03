415ee6ae0866762daae27d78373f0625f02d5913
Arnold138 <belhocine.arnold@gmail.com>
2025-06-04 01:33:55 +0200

    Ajout de la page d'erreur 404 avec son css et sa redirection vers la page d'accueil

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import Nav from "../../components/nav";
+import Footer from "../../components/Footer";
+import { NavLink } from "react-router-dom";
+import '../../styles/Error.scss';
 export default function Error() {
   return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
+    
+    <div className="error-page">
+      <Nav /> 
       <h1>404</h1>
-      <p>Cette page n’existe pas.</p>
+      <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+      <NavLink to="/" className="error-link">Retourner sur la page d'accueil</NavLink>
+      <Footer />
     </div>
+   
+
   );
 }
